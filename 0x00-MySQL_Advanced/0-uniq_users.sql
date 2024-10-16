b49ff1d471c43fe4de4ade859150484e1985d6cb
Wiraama <mawira01@students.uonbi.ac.ke>
2024-10-16 21:05:37 +0300

    readme and task 1

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- script to create table user with the requirements
+-- if table exist it will fail
+-- to be executed on any database
+CREATE TABLE IF NOT EXISTS user (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY_KEY,
+	email VARCHAR(255) UNIQUE NOT NULL,
+	name VARCHAR(255)
+);
