759a1410d02c043b9f0c2811b428780f7640260e
Wiraama <mawira01@students.uonbi.ac.ke>
2024-10-17 02:37:29 +0300

    3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,10 +1,5 @@
 -- some shit
-SELECT
-	band_name,
-	(2022 - formed) AS lifespan
-FROM
-	metal_bands
-WHERE
-	main_style = 'Glam rock'
-ORDER BY
-	lifespan DESC;
+SELECT band_name, (2022 - formed) AS lifespan
+FROM metal_bands
+WHERE main_style = 'Glam rock'
+ORDER BY lifespan DESC;
