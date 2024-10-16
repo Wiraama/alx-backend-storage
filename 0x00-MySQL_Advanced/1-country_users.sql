4d6933ad209c9ecaf841c2cae6a78cb9a35c5b2b
Wiraama <mawira01@students.uonbi.ac.ke>
2024-10-17 02:24:49 +0300

    1-country_users.sql

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,6 +1,4 @@
 -- script to  create a table
--- if exist fail
--- executed in any device
 CREATE TABLE IF NOT EXISTS user (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
