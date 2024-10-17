309db359d035d365e6632116f1a557acc165ee88
Wiraama <mawira01@students.uonbi.ac.ke>
2024-10-17 03:11:43 +0300

    3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,3 @@
 -- some shit
 SELECT band_name, (IFFULL(split, 2022) - formed)
-AS lifespan FROM metal_bands WHERE main_style LIKE '%Glam rock%' ORDER BY lifespan DESC;
+AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
