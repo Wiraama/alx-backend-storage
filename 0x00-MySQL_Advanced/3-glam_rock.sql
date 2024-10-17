3724c1707a0f55433c3fefa452fd66871a8a3573
Wiraama <mawira01@students.uonbi.ac.ke>
2024-10-17 03:14:10 +0300

    3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,3 @@
--- some shit
-SELECT band_name, (IFFULL(split, 2022) - formed)
+-- Lists all bands with Glam rock as their main style, ranked by their longevity
+SELECT band_name, (IFNULL(split, 2022) - formed)
 AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
