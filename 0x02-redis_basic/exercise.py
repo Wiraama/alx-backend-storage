14397c5aee484a611e3affc62473a82a120dacd0
Wiraama <mawira01@students.uonbi.ac.ke>
2024-10-25 01:27:01 +0300

    all

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """ ... """
+from functools import wraps
 import redis
 import uuid
 from typing import Union, Callable, Optional
@@ -21,6 +22,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
