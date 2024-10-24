b122b132b698d71f0172e3bfb67e4bdf18dbfa88
Wiraama <mawira01@students.uonbi.ac.ke>
2024-10-24 13:41:30 +0300

    -m

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Write a Python function that inserts a new document in a collection based on kwargs """
+
+
+def insert_school(mongo_collection, **kwargs):
+    docs = mongo_collection.insert_one(kwargs)
+    return docs.inserted_id
