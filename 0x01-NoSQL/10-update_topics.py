bc82de9cd1de2c9f65d80d59c15d2fc4c8d04461
Wiraama <mawira01@students.uonbi.ac.ke>
2024-10-24 14:00:31 +0300

    10-update_topics.py

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -8,4 +8,4 @@ def update_topics(mongo_collection, name, topics):
     docs = mono_collection.update_many(
             {name: "name"},
             {$set: {topics: "topics"}}
-            )
+        )
