a80034315cf7c2efec9eed73809774b75d56c31b
Wiraama <mawira01@students.uonbi.ac.ke>
2024-10-24 22:59:48 +0300

    all

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" ... """
+import redis
+import uuid
+from typing import Union
+
+class Cache:
+    """ class Cache """
+    def __init__(self):
+        """ initializing cache class """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
